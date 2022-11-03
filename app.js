b05853245458b2277b5d515df27854e3c6bc18cb
Ben Greanya <bengreanya@gmail.com>
2022-11-03 15:01:52 -0700

    added DOM elements dropdown images and slogan

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-/* Imports */
-
 /* Get DOM Elements */
+//grabbed dropdowns
+const dropdownClimate = document.getElementById('climate-dropdown');
+const dropdownArchitecture = document.getElementById('architecture-dropdown');
+const dropdownFood = document.getElementById('food-dropdown');
+
+//grabbed images
+const climateImg = document.getElementById('climateImage');
+const architectureImg = document.getElementById('architectureImage');
+const foodImg = document.getElementById('foodImage');
+
+// add slogan section
+const sloganInput = document.getElementsBy('slogan-text');
+const inputButton = document.getElementById('submit');
+const sloganList = document.getElementById('slogan-display');
 
 /* State */
 
