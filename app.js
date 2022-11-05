668d875085010a6cd570ebc212e197d83025a8e8
MontanoDiego <diego.e2.montano@gmail.com>
2022-11-04 17:17:09 -0700

    added state variables and display stats function

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const sloganList = document.getElementById('slogan-display');
 // grab stat elements
 const climateElement = document.getElementById('climate-stat');
 const archElement = document.getElementById('arch-stat');
-const foodElement = document.getElementById('food-stat');
+const foodElement = document.getElementById('cuisine-stat');
 const sloganElement = document.getElementById('slogan-stat');
 
 /* State */
@@ -27,6 +27,7 @@ let slogans = [];
 let climateCounter = 0;
 let architectureCounter = 0;
 let foodCounter = 0;
+let sloganCounter = 0;
 
 /* Events */
 
@@ -34,25 +35,29 @@ dropdownClimate.addEventListener('change', (e) => {
     const value = e.target.value;
     climateImg.src = `./assets/${value}-climate.png`;
     climateCounter++;
+    displayStats();
 });
 
 dropdownArchitecture.addEventListener('change', (e) => {
     const value = e.target.value;
     architectureImg.src = `./assets/${value}-architecture.png`;
     architectureCounter++;
+    displayStats();
 });
 
 dropdownFood.addEventListener('change', (e) => {
     const value = e.target.value;
     foodImg.src = `./assets/${value}-food.png`;
     foodCounter++;
+    displayStats();
 });
 
 inputButton.addEventListener('click', () => {
     slogans.push(sloganInput.value);
     displaySlogans();
-    console.log(slogans);
     sloganInput.value = '';
+    sloganCounter++;
+    displayStats();
 });
 
 /* Display Functions */
@@ -66,7 +71,10 @@ function displaySlogans() {
 }
 
 function displayStats() {
-
+    climateElement.textContent = climateCounter;
+    archElement.textContent = architectureCounter;
+    foodElement.textContent = foodCounter;
+    sloganElement.textContent = sloganCounter;
 }
 
 // (don't forget to call any display functions you want to run on page load!)
